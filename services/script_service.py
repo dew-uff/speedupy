a1941ee24187295920b4e23da7f6258c7de7d8b4
JoaoLopez <joaolopez@id.uff.br>
2024-01-09 12:47:20 -0300

    Permitindo cópia de experimentos utilizando caminho relativo e imports implícitos de arquivos __init__.py

--- services/script_service.py
+++ services/script_service.py
@@ -1,4 +1,4 @@
-import ast, os
+import ast, os, re
 
 from typing import List, Dict, Union
 from util import python_code_to_AST, get_script_path, script_name_to_script_path, is_an_user_defined_script
@@ -39,7 +39,7 @@ def decorate_script_functions(script:Script, classified_functions:Dict[str, Func
         decorate_function(function, script.function_graph, classified_functions)
 
 def copy_script(script:Script, exp_base_dir:str):
-    __update_import_command_paths(script.import_commands, os.path.dirname(script.name), exp_base_dir)
+    __update_import_command_paths(script, exp_base_dir)
     script.name = __get_script_temp_path(script.name)
     if __script_is_inside_folder(script.name):
         __create_script_path(script.name)
@@ -47,16 +47,16 @@ def copy_script(script:Script, exp_base_dir:str):
     with open(script.name, "wt") as f:
         f.write(ast.unparse(script.AST))
 
-def __update_import_command_paths(import_commands:List[Union[ast.Import, ast.ImportFrom]], main_script_folder:str, exp_base_dir:str):
-    for imp in import_commands:
+def __update_import_command_paths(script:Script, exp_base_dir:str):
+    for imp in script.import_commands:
         if isinstance(imp, ast.Import):
-            __update_ast_Import_command_paths(imp, main_script_folder, exp_base_dir)
+            __update_ast_Import_command_paths(script, imp, exp_base_dir)
         elif isinstance(imp, ast.ImportFrom):
-            __update_ast_ImportFrom_command_paths(imp, main_script_folder, exp_base_dir)
+            __update_ast_ImportFrom_command_paths(script, imp, exp_base_dir)
 
-def __update_ast_Import_command_paths(import_command:ast.Import, main_script_folder:str, exp_base_dir:str) -> None:
+def __update_ast_Import_command_paths(script:Script, import_command:ast.Import, exp_base_dir:str) -> None:
     for alias in import_command.names:
-        if __path_need_to_be_updated(alias.name, main_script_folder, exp_base_dir):
+        if __path_need_to_be_updated(alias.name, os.path.dirname(script.name), exp_base_dir):
             alias.name = get_updated_path(alias.name)
 
 def __path_need_to_be_updated(imported_script_path:str, main_script_folder:str, exp_base_dir:str) -> bool:
@@ -67,24 +67,29 @@ def get_updated_path(old_path:str) -> str:
     new_path = old_path.replace(".", "_temp.")
     return new_path + "_temp"
 
-def __update_ast_ImportFrom_command_paths(import_command:ast.ImportFrom, main_script_folder:str, exp_base_dir:str) -> None:
+def __update_ast_ImportFrom_command_paths(script:Script, import_command:ast.ImportFrom, exp_base_dir:str) -> None:
     imported_script_name = import_command.level * "." + import_command.module if import_command.module is not None else import_command.level * "."
-    if __path_need_to_be_updated(imported_script_name, main_script_folder, exp_base_dir):
+    if __path_need_to_be_updated(imported_script_name, os.path.dirname(script.name), exp_base_dir):
         #from .... import FUNCTION
         if import_command.module is not None:
             import_command.module = get_updated_path(import_command.module)
     else:
         #Checking if script is using "from .... import MODULE (........) MODULE.FUNCTION()"
         for alias in import_command.names:
-            complete_path = ".".join([imported_script_name, alias.name])
-            if __path_need_to_be_updated(complete_path, main_script_folder, exp_base_dir):
+            if imported_script_name[-1] == '.': #imported_script_name is only a relative path
+                sep = ''
+            else:
+                sep = '.'
+            complete_path = sep.join([imported_script_name, alias.name])
+            if __path_need_to_be_updated(complete_path, os.path.dirname(script.name), exp_base_dir):
                 if import_command.module is not None:
                     import_command.module = get_updated_path(import_command.module)
                 alias.name += "_temp"
 
 def __get_script_temp_path(script_path:str) -> str:
     folders, script_name = os.path.split(script_path)
-    script_name = script_name.replace(".py", "_temp.py")
+    if script_name != '__init__.py':
+        script_name = script_name.replace(".py", "_temp.py")
     if __script_is_inside_folder(script_path):
         temp_path = (os.sep).join([f"{fol}_temp" for fol in folders.split(os.sep)])
         return os.path.join(temp_path, script_name)
