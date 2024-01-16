b420af47192c3166cb2fb1afb87e6977bcfffe83
JoaoLopez <joaolopez@id.uff.br>
2024-01-16 15:35:27 -0300

    Adicionando testes unitários para @deterministic, @maybe_deterministic e @collect_metadata quando executa-se o speedupy com a opção --no-cache

--- test/test_intpy.py
+++ test/test_intpy.py
@@ -1,39 +1,58 @@
 import unittest, os, sys, ast
+import importlib
 from unittest.mock import patch, Mock
-
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
 from constantes import Constantes
-from intpy import _function_call_maybe_deterministic
+import intpy
+
 class TestIntPy(unittest.TestCase):
+    def test_deterministic_when_executing_speedupy_with_no_cache_arg(self):
+        Constantes().g_argsp_no_cache = True
+        importlib.reload(intpy)
+        self.assertEqual(func, intpy.deterministic(func))
+        
+    def test_maybe_deterministic_when_executing_speedupy_with_no_cache_arg(self):
+        Constantes().g_argsp_no_cache = True
+        importlib.reload(intpy)
+        self.assertEqual(func, intpy.maybe_deterministic(func))
+
+    def test_collect_metadata_when_executing_speedupy_with_no_cache_arg(self):
+        Constantes().g_argsp_no_cache = True
+        importlib.reload(intpy)
+        self.assertEqual(func, intpy.collect_metadata(func))
+
     def test_function_call_maybe_deterministic_when_DONT_CACHE_FUNCTION_CALLS_is_empty(self):
-        def func(x, y):
-            return x / y
         Constantes().g_argsp_no_cache = False
+        importlib.reload(intpy)
+        
         Constantes().FUNCTIONS_2_HASHES = {func.__qualname__:"function_hash"}
         Constantes().DONT_CACHE_FUNCTION_CALLS = []
         with patch('intpy.get_id', return_value='function_call_hash'):
-            self.assertTrue(_function_call_maybe_deterministic(func, [1, 2], {}))
+            self.assertTrue(intpy._function_call_maybe_deterministic(func, [1, 2], {}))
     
     def test_function_call_maybe_deterministic_when_function_call_in_DONT_CACHE_FUNCTION_CALLS(self):
-        def func(x, y):
-            return x / y
         Constantes().g_argsp_no_cache = False
+        importlib.reload(intpy)
+        
         Constantes().FUNCTIONS_2_HASHES = {func.__qualname__:"function_hash"}
         Constantes().DONT_CACHE_FUNCTION_CALLS = ['function_call_hash']
         with patch('intpy.get_id', return_value='function_call_hash'):
-            self.assertFalse(_function_call_maybe_deterministic(func, [1, 2], {}))
+            self.assertFalse(intpy._function_call_maybe_deterministic(func, [1, 2], {}))
 
     def test_function_call_maybe_deterministic_when_function_call_not_in_DONT_CACHE_FUNCTION_CALLS(self):
-        def func(x, y):
-            return x / y
         Constantes().g_argsp_no_cache = False
+        importlib.reload(intpy)
+        
         Constantes().FUNCTIONS_2_HASHES = {func.__qualname__:"function_hash"}
         Constantes().DONT_CACHE_FUNCTION_CALLS = ['fc_hash_1', 'fc_hash_2', 'fc_hash_3']
         with patch('intpy.get_id', return_value='function_call_hash'):
-            self.assertTrue(_function_call_maybe_deterministic(func, [1, 2], {}))    
+            self.assertTrue(intpy._function_call_maybe_deterministic(func, [1, 2], {}))    
+
+def func(x, y):
+    return x / y
 
 if __name__ == '__main__':
     unittest.main()
