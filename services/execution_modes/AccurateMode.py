9aa0f646fd0aef23b4e03150dc604432530842dc
JoaoLopez <joaolopez@id.uff.br>
2024-02-28 12:50:47 -0300

    Adicionando testes unitários para a classe AccurateMode

--- services/execution_modes/AccurateMode.py
+++ services/execution_modes/AccurateMode.py
@@ -3,8 +3,9 @@ from data_access import get_func_call_prov
 
 class AccurateMode(AbstractExecutionMode):
     def func_call_can_be_cached(self, func_call_hash:str) -> bool:
-        self.__func_call_prov = get_func_call_prov(func_call_hash)
-        return len(self.__func_call_prov.outputs) == 1
+        func_call_prov = get_func_call_prov(func_call_hash)
+        return len(func_call_prov.outputs) == 1
     
     def get_func_call_cache(self, func_call_hash:str):
-        return self.__func_call_prov.outputs[0]['value']
+        func_call_prov = get_func_call_prov(func_call_hash)
+        return func_call_prov.outputs[0]['value']
