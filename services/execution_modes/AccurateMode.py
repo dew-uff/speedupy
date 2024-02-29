c5d270abbc05c53bb25be07ae716480384056ad2
JoaoLopez <joaolopez@id.uff.br>
2024-02-29 16:09:09 -0300

    Adaptando AccurateMode para novo formato de FunctionCallProv.outputs

--- services/execution_modes/AccurateMode.py
+++ services/execution_modes/AccurateMode.py
@@ -1,5 +1,6 @@
 from services.execution_modes.AbstractExecutionMode import AbstractExecutionMode
 from data_access import get_func_call_prov
+from pickle import loads
 
 class AccurateMode(AbstractExecutionMode):
     def func_call_can_be_cached(self, func_call_hash:str) -> bool:
@@ -8,4 +9,4 @@ class AccurateMode(AbstractExecutionMode):
     
     def get_func_call_cache(self, func_call_hash:str):
         func_call_prov = get_func_call_prov(func_call_hash)
-        return func_call_prov.outputs[0]['value']
+        return loads(list(func_call_prov.outputs.keys())[0])
--- test/test_services/test_execution_modes/test_AccurateMode.py
+++ test/test_services/test_execution_modes/test_AccurateMode.py
@@ -1,4 +1,5 @@
 import unittest, os, sys
+from pickle import dumps, loads
 from unittest.mock import patch
 
 project_folder = os.path.realpath(__file__).split('test/')[0]
@@ -14,37 +15,53 @@ class TestAccurateMode(unittest.TestCase):
         self.get_func_call_prov_namespace = 'services.execution_modes.AccurateMode.get_func_call_prov'
     
     def test_func_call_can_be_cached_when_func_has_one_output(self):
-        self.function_call_prov._FunctionCallProv__outputs = [{'value':1, 'freq':12}]
+        self.function_call_prov._FunctionCallProv__outputs = {dumps(1):12}
         with patch(self.get_func_call_prov_namespace, return_value=self.function_call_prov) as get_func_call_prov:
             self.assertTrue(self.accurateMode.func_call_can_be_cached('func_call_hash'))
             get_func_call_prov.assert_called_once()
     
     def test_func_call_can_be_cached_when_func_has_2_outputs(self):
-        self.function_call_prov._FunctionCallProv__outputs = [{'value':2, 'freq':5},
-                                                              {'value':3, 'freq':3}]
+        self.function_call_prov._FunctionCallProv__outputs = {dumps(2): 5,
+                                                              dumps(3): 3}
         with patch(self.get_func_call_prov_namespace, return_value=self.function_call_prov) as get_func_call_prov:
             self.assertFalse(self.accurateMode.func_call_can_be_cached('func_call_hash'))
             get_func_call_prov.assert_called_once()
 
     def test_func_call_can_be_cached_when_func_has_many_outputs(self):
-        self.function_call_prov._FunctionCallProv__outputs = [{'value':1, 'freq':12},
-                                                              {'value':2, 'freq':5},
-                                                              {'value':3, 'freq':3},
-                                                              {'value':4, 'freq':1}]
+        self.function_call_prov._FunctionCallProv__outputs = {dumps(1):12,
+                                                              dumps(2):5,
+                                                              dumps(3):3,
+                                                              dumps(4):1}
         with patch(self.get_func_call_prov_namespace, return_value=self.function_call_prov) as get_func_call_prov:
             self.assertFalse(self.accurateMode.func_call_can_be_cached('func_call_hash'))
             get_func_call_prov.assert_called_once()
     
     def test_get_func_call_cache_with_different_types(self):
-        self.function_call_prov._FunctionCallProv__outputs = [{'value':1, 'freq':12}]
+        self.function_call_prov._FunctionCallProv__outputs = {dumps(1):12}
         with patch(self.get_func_call_prov_namespace, return_value=self.function_call_prov) as get_func_call_prov:
             self.assertEqual(self.accurateMode.get_func_call_cache('func_call_hash'), 1)
             get_func_call_prov.assert_called_once()
 
-        self.function_call_prov._FunctionCallProv__outputs = [{'value':'my_result', 'freq':12}]
+        self.function_call_prov._FunctionCallProv__outputs = {dumps('my_result'):12}
         with patch(self.get_func_call_prov_namespace, return_value=self.function_call_prov) as get_func_call_prov:
             self.assertEqual(self.accurateMode.get_func_call_cache('func_call_hash'), 'my_result')
             get_func_call_prov.assert_called_once()
+
+        self.function_call_prov._FunctionCallProv__outputs = {dumps({1, 2, 3}):12}
+        with patch(self.get_func_call_prov_namespace, return_value=self.function_call_prov) as get_func_call_prov:
+            self.assertEqual(self.accurateMode.get_func_call_cache('func_call_hash'), {1, 2, 3})
+            get_func_call_prov.assert_called_once()
+
+        self.function_call_prov._FunctionCallProv__outputs = {dumps(MyClass()):12}
+        with patch(self.get_func_call_prov_namespace, return_value=self.function_call_prov) as get_func_call_prov:
+            self.assertEqual(dumps(self.accurateMode.get_func_call_cache('func_call_hash')),
+                             dumps(MyClass()))
+            get_func_call_prov.assert_called_once()
+
+class MyClass():
+    def __init__(self):
+        self.__x = 10
+        self.__y = 20
 
 if __name__ == '__main__':
     unittest.main()
