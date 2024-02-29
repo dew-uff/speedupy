c6f80739c8a1616d4b10b2b8f12eccad072ebdf8
JoaoLopez <joaolopez@id.uff.br>
2024-02-29 15:31:52 -0300

    Refatorando o método que calcula a sequência ponderada que substitui uma função acelerada pelo SpeeduPy no modo ProbabilisticFrequencyMode

--- services/execution_modes/ProbabilisticFrequencyMode.py
+++ services/execution_modes/ProbabilisticFrequencyMode.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple, List
+
 from services.execution_modes.AbstractExecutionMode import AbstractExecutionMode
 from services.execution_modes.util import func_call_mode_output_occurs_enough
 from data_access import get_func_call_prov
@@ -23,44 +25,58 @@ class ProbabilisticFrequencyMode(AbstractExecutionMode):
             self.__func_call_prov.next_index_weighted_seq = 0
         return self.__func_call_prov.weighted_output_seq[index]
     
-    #TODO Necessário adicionar muitos testes unitários!!!
     def _calculate_weighted_output_seq(self):
-        mode_abs_freq = None
+        mode_abs_freq = self.__get_mode_abs_freq()
+        output_weights = self.__get_function_output_weights_based_on_the_mode_freq(mode_abs_freq)
+        begin_seq, num_occurences_outputs_on_seq = self.__get_beginning_of_output_seq_interleaved_with_statistical_mode(mode_abs_freq, output_weights)
+        remaining_outputs = self.__get_remaining_outputs_on_weighted_seq(num_occurences_outputs_on_seq)
+        end_seq = self.__get_end_of_output_seq_with_interleaved_remaining_outputs(remaining_outputs)
+        self.__func_call_prov.weighted_output_seq = begin_seq + end_seq
+
+    def __get_mode_abs_freq(self) -> int:
         for output in self.__func_call_prov.outputs:
             if output['value'] == self.__func_call_prov.mode_output:
-                mode_abs_freq = output['freq']
-                break
-        
+                return output['freq']
+            
+    def __get_function_output_weights_based_on_the_mode_freq(self, mode_abs_freq:int) -> Dict:
         output_weights = {}
         for output in self.__func_call_prov.outputs:
             if output['value'] == self.__func_call_prov.mode_output: continue
             key = pickle.dumps(output['value'])
             output_weights[key] = ceil(mode_abs_freq/output['freq'])
-
+        return output_weights
+    
+    def __get_beginning_of_output_seq_interleaved_with_statistical_mode(self, mode_abs_freq:int , output_weights:Dict) -> Tuple[List, Dict]:
         aux = deepcopy(output_weights)
         num_occurences_outputs_on_seq = {output:0 for output in output_weights.keys()}
-        self.__func_call_prov.weighted_output_seq = []
+        seq = []
         for i in range(mode_abs_freq):
-            self.__func_call_prov.weighted_output_seq.append(self.__func_call_prov.mode_output)
-            for output_value in aux:
-                aux[output_value] -= 1
-                if aux[output_value] == 0:
-                    self.__func_call_prov.weighted_output_seq.append(pickle.loads(output_value))
-                    num_occurences_outputs_on_seq[output_value] += 1
-                    aux[output_value] = output_weights[output_value]
-
+            seq.append(self.__func_call_prov.mode_output)
+            for output in aux:
+                aux[output] -= 1
+                if aux[output] == 0:
+                    seq.append(pickle.loads(output))
+                    num_occurences_outputs_on_seq[output] += 1
+                    aux[output] = output_weights[output]
+        return seq, num_occurences_outputs_on_seq
+    
+    def __get_remaining_outputs_on_weighted_seq(self, num_occurences_each_output:Dict) -> Dict:
         remaining_outputs = {}
         for output in self.__func_call_prov.outputs:
             if(output['value'] == self.__func_call_prov.mode_output): continue
             key = pickle.dumps(output['value'])
-            if(output['freq'] > num_occurences_outputs_on_seq[key]):
-                remaining_outputs[key] = output['freq'] - num_occurences_outputs_on_seq[key]
+            if(output['freq'] > num_occurences_each_output[key]):
+                remaining_outputs[key] = output['freq'] - num_occurences_each_output[key]
+        return remaining_outputs
 
+    def __get_end_of_output_seq_with_interleaved_remaining_outputs(self, remaining_outputs:Dict) -> List:
+        seq = []
         while len(remaining_outputs) > 0:
             aux = {}
             for output_value in remaining_outputs:
-                self.__func_call_prov.weighted_output_seq.append(pickle.loads(output_value))
+                seq.append(pickle.loads(output_value))
                 remaining_outputs[output_value] -= 1
                 if remaining_outputs[output_value] > 0:
                     aux[output_value] = remaining_outputs[output_value]
             remaining_outputs = aux
+        return seq
