6d5c27c1358be6cb0a551c278e8fd166f34cc869
JoaoLopez <joaolopez@id.uff.br>
2024-03-06 12:48:45 -0300

    Atualizando módulo script_service e seus testes unitários para funcionarem com a separação do setup da execução do speedupy

--- services/script_service.py
+++ services/script_service.py
@@ -1,13 +1,12 @@
 import ast, os, importlib
 
-from typing import List, Dict, Union
+from typing import List, Dict
 from util import python_code_to_AST, get_script_path
 from entities.Script import Script
 from entities.Experiment import Experiment
-from services.ASTSearcher import ASTSearcher
-from services.ScriptFunctionGraphCreator import ScriptFunctionGraphCreator
+from setup_exp.services.ASTSearcherService import ASTSearcherService
+from setup_exp.services.ScriptFunctionGraphCreatorService import ScriptFunctionGraphCreatorService
 from services.function_service import decorate_function, execute_and_classify_function
-from constantes import Constantes
 
 def create_script(script_name:str, experiment_base_dir:str) -> Script:
     script_path = get_script_path(script_name, experiment_base_dir)
@@ -15,7 +14,7 @@ def create_script(script_name:str, experiment_base_dir:str) -> Script:
     if(script_AST is None):
         raise RuntimeError
 
-    script_ASTSearcher = ASTSearcher(script_AST)
+    script_ASTSearcher = ASTSearcherService(script_AST)
     script_ASTSearcher.search()
 
     for function_name in script_ASTSearcher.functions:
@@ -26,28 +25,35 @@ def create_script(script_name:str, experiment_base_dir:str) -> Script:
 def create_script_function_graph(main_script:Script, experiment:Experiment) -> None:
     u_def_imported_scripts = main_script.get_user_defined_imported_scripts(experiment.base_dir)
     __create_user_defined_imported_scripts_function_graphs(u_def_imported_scripts, experiment)
-    script_function_graph = ScriptFunctionGraphCreator(main_script, u_def_imported_scripts, experiment).create_function_graph()
+    script_function_graph = ScriptFunctionGraphCreatorService(main_script, u_def_imported_scripts, experiment).create_function_graph()
     main_script.function_graph = script_function_graph
 
 def __create_user_defined_imported_scripts_function_graphs(u_def_imported_scripts:List[str], experiment:Experiment) -> None:
     for script_name in u_def_imported_scripts:
         create_script_function_graph(experiment.scripts[script_name], experiment)
 
-def decorate_script_functions_for_execution(script:Script) -> None:
+def decorate_script_functions_for_execution(script:Script) -> bool:
+    """This function decorates all functions of a script that can be decorated with @maybe_deterministic. It returns True if at least one function was decorated. If no functions were marked with @maybe_deterministic, this function returns False."""
+    script_decorated = False
     for function in script.functions.values():
-        decorate_function(function)
+        function_decorated = decorate_function(function)
+        script_decorated = script_decorated or function_decorated
+    return script_decorated
 
-def add_common_decorator_imports_for_execution(script:Script) -> None:
+def add_decorator_import(script:Script, decorator_name:str) -> None:
+    if _script_already_has_decorator_import(script, decorator_name): return
     current_folder = os.getcwd()
     imports = f"import sys\nsys.path.append('{current_folder}')\n"
-    imports += "from speedupy.intpy import maybe_deterministic"
+    imports += f"from speedupy.speedupy import {decorator_name}"
     imports = ast.parse(imports)
     script.AST.body = imports.body + script.AST.body
 
-def add_execute_intpy_import(script:Script) -> None:
-    import_command = ast.parse("from speedupy.intpy import execute_intpy")
-    #Inserting import right after "from speedupy.intpy import maybe_deterministic"
-    script.AST.body.insert(3, import_command)
+def _script_already_has_decorator_import(script:Script, decorator_name:str) -> bool:
+    for cmd in script.import_commands:
+        if isinstance(cmd, ast.ImportFrom) and cmd.module == 'speedupy.speedupy' and cmd.level == 0:
+            for alias in cmd.names:
+                if alias.name == decorator_name: return True
+    return False
 
 def add_start_inference_engine_import(script:Script) -> None:
     #Replacing "from speedupy.intpy import execute_intpy" for 
@@ -56,11 +62,8 @@ def add_start_inference_engine_import(script:Script) -> None:
     import_command = ast.parse("from speedupy.function_inference_engine import start_inference_engine")
     script.AST.body.insert(3, import_command)
 
-def copy_script(script:Script):        
-    folders = os.path.dirname(script.name)
-    temp_path = os.path.join(Constantes().TEMP_FOLDER, folders)
-    os.makedirs(temp_path, exist_ok=True)
-    with open(os.path.join(Constantes().TEMP_FOLDER, script.name), 'wt') as f:
+def overwrite_decorated_script(script:Script):        
+    with open(script.name, 'wt') as f:
         f.write(ast.unparse(script.AST))
 
 def classify_script_functions(script:Script, functions_2_hashes:Dict[str, str]) -> None:
--- test/test_services/test_script_service.py
+++ test/test_services/test_script_service.py
@@ -1,13 +1,13 @@
 import unittest, unittest.mock, os, sys, ast
+from unittest.mock import patch
 
 project_folder = os.path.realpath(__file__).split('test/')[0]
 sys.path.append(project_folder)
 
-from services.script_service import copy_script, add_common_decorator_imports_for_execution, add_start_inference_engine_import, add_execute_intpy_import, _get_module_name
+from services.script_service import overwrite_decorated_script, add_decorator_import, add_start_inference_engine_import, decorate_script_functions_for_execution, _get_module_name
 from constantes import Constantes
 from entities.Script import Script
 from entities.FunctionGraph import FunctionGraph
-
 
 class TestScriptService(unittest.TestCase):
     @classmethod
@@ -22,8 +22,7 @@ class TestScriptService(unittest.TestCase):
                              'folder',
                              'folder2',
                              'folder3',
-                             'folder4',
-                             '.intpy_temp']
+                             'folder4']
         for f in files_and_folders:
             if os.path.exists(f):
                 os.system(f'rm -rf {f}')
@@ -37,20 +36,18 @@ class TestScriptService(unittest.TestCase):
     def normalize_string(self, string):
         return string.replace("\n\n", "\n").replace("\t", "    ").replace("\"", "'")
 
-    def assert_script_was_correctly_copied(self):
-        copied_script_path = os.path.join(Constantes().TEMP_FOLDER, self.script.name)
-        self.assertTrue(os.path.exists(copied_script_path))
-        with open(self.script.name) as f1:
-            with open(copied_script_path) as f2:
-                code1 = self.normalize_string(f1.read())
-                code2 = self.normalize_string(f2.read())
-                self.assertEqual(code1, code2)
+    def assert_script_was_written_as_expected(self, expected_code:str):
+        with open(self.script.name) as f:
+            actual_code = f.read()
+        code1 = self.normalize_string(expected_code)
+        code2 = self.normalize_string(actual_code)
+        self.assertEqual(code1, code2)
     
-    def test_copy_script_without_changing_its_AST(self):
+    def test_decorate_script_functions_for_execution_when_no_function_was_decorated(self):
         with open('script_test.py', 'wt') as f:
             f.write('@deterministic\ndef f1(a, b, c=10):\n\ta * b / c\n')
             f.write('@collect_metadata\ndef f2():\n\tdef f21(x, y=3):\n\t\tdef f211(a):\n\t\t\treturn "f211"\n\t\treturn "f21"\n\treturn "f2"\n')
-            f.write('@initialize_intpy(__file__)\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
+            f.write('@initialize_speedupy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
             f.write('main()')
         fileAST = self.getAST('script_test.py')
         functions = {'f1':fileAST.body[0],
@@ -59,15 +56,24 @@ class TestScriptService(unittest.TestCase):
                      'f2.<locals>.f21.<locals>.f211':fileAST.body[1].body[0].body[0],
                      'main':fileAST.body[2]}
         self.script = Script('script_test.py', fileAST, [], functions)
-                
-        copy_script(self.script)
-        self.assert_script_was_correctly_copied()
+        with patch('services.script_service.decorate_function', return_value=False):
+            self.assertFalse(decorate_script_functions_for_execution(self.script))
 
-    def test_copy_script_after_changing_its_AST(self):
+    def test_decorate_script_functions_for_execution_when_a_function_was_decorated(self):
         with open('script_test.py', 'wt') as f:
-            f.write('def f1(a, b, c=10):\n\ta * b / c\n')
+            f.write('@initialize_speedupy\ndef main():\n\treturn 10\n')
+            f.write('main()')
+        fileAST = self.getAST('script_test.py')
+        functions = {'main':fileAST.body[0]}
+        self.script = Script('script_test.py', fileAST, [], functions)
+        with patch('services.script_service.decorate_function', return_value=True):
+            self.assertTrue(decorate_script_functions_for_execution(self.script))
+
+    def test_decorate_script_functions_for_execution_when_many_functions_were_decorated(self):
+        with open('script_test.py', 'wt') as f:
+            f.write('@deterministic\ndef f1(a, b, c=10):\n\ta * b / c\n')
             f.write('@collect_metadata\ndef f2():\n\tdef f21(x, y=3):\n\t\tdef f211(a):\n\t\t\treturn "f211"\n\t\treturn "f21"\n\treturn "f2"\n')
-            f.write('@initialize_intpy(__file__)\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
+            f.write('@initialize_speedupy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
             f.write('main()')
         fileAST = self.getAST('script_test.py')
         functions = {'f1':fileAST.body[0],
@@ -75,28 +81,57 @@ class TestScriptService(unittest.TestCase):
                      'f2.<locals>.f21':fileAST.body[1].body[0],
                      'f2.<locals>.f21.<locals>.f211':fileAST.body[1].body[0].body[0],
                      'main':fileAST.body[2]}
-        script = Script('script_test.py', fileAST, [], functions)
-        functions['f1'].decorator_list.append(ast.Name(id='decorator1', ctx=ast.Load()))
-        functions['f2'].decorator_list.append(ast.Name(id='decorator2', ctx=ast.Load()))
-        functions['main'].decorator_list[0] = ast.Name(id='execute_intpy', ctx=ast.Load())
-                
-        copy_script(script)
-        copied_script_path = os.path.join(Constantes().TEMP_FOLDER, script.name)
-        self.assertTrue(os.path.exists(copied_script_path))
-        with open(copied_script_path) as f2:
-            code1 = '@decorator1\ndef f1(a, b, c=10):\n\ta * b / c\n'
-            code1 += '@collect_metadata\n@decorator2\ndef f2():\n\tdef f21(x, y=3):\n\t\tdef f211(a):\n\t\t\treturn "f211"\n\t\treturn "f21"\n\treturn "f2"\n'
-            code1 += '@execute_intpy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n'
-            code1 += 'main()'
-            code1 = self.normalize_string(code1)
-            code2 = self.normalize_string(f2.read())
-            self.assertEqual(code1, code2)    
-    
-    def test_add_common_decorator_imports_for_execution_to_script(self):
+        self.script = Script('script_test.py', fileAST, [], functions)
+        with patch('services.script_service.decorate_function', return_value=True):
+            self.assertTrue(decorate_script_functions_for_execution(self.script))
+
+    def test_overwrite_decorated_script_without_changing_its_AST(self):
         with open('script_test.py', 'wt') as f:
-            f.write('@deterministic\ndef f1(a, b, c=10):\n\treturn a * b / c\n')
+            f.write('@deterministic\ndef f1(a, b, c=10):\n\ta * b / c\n')
+            f.write('@collect_metadata\ndef f2():\n\tdef f21(x, y=3):\n\t\tdef f211(a):\n\t\t\treturn "f211"\n\t\treturn "f21"\n\treturn "f2"\n')
+            f.write('@initialize_speedupy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
+            f.write('main()')
+        with open('script_test.py', 'rt') as f: original_script = f.read()
+        fileAST = self.getAST('script_test.py')
+        functions = {'f1':fileAST.body[0],
+                     'f2':fileAST.body[1],
+                     'f2.<locals>.f21':fileAST.body[1].body[0],
+                     'f2.<locals>.f21.<locals>.f211':fileAST.body[1].body[0].body[0],
+                     'main':fileAST.body[2]}
+        self.script = Script('script_test.py', fileAST, [], functions)
+
+        overwrite_decorated_script(self.script)
+        self.assert_script_was_written_as_expected(original_script)
+
+    def test_overwrite_decorated_script_after_changing_its_AST(self):
+        with open('script_test.py', 'wt') as f:
+            f.write('def f1(a, b, c=10):\n\ta * b / c\n')
+            f.write('@decorator1\ndef f2():\n\tdef f21(x, y=3):\n\t\tdef f211(a):\n\t\t\treturn "f211"\n\t\treturn "f21"\n\treturn "f2"\n')
+            f.write('@initialize_speedupy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
+            f.write('main()')
+        fileAST = self.getAST('script_test.py')
+        functions = {'f1':fileAST.body[0],
+                     'f2':fileAST.body[1],
+                     'f2.<locals>.f21':fileAST.body[1].body[0],
+                     'f2.<locals>.f21.<locals>.f211':fileAST.body[1].body[0].body[0],
+                     'main':fileAST.body[2]}
+        self.script = Script('script_test.py', fileAST, [], functions)
+        functions['f1'].decorator_list.append(ast.Name(id='deterministic', ctx=ast.Load()))
+        functions['f2'].decorator_list.append(ast.Name(id='maybe_deterministic', ctx=ast.Load()))
+        functions['main'].decorator_list[0] = ast.Name(id='test', ctx=ast.Load())
+                
+        overwrite_decorated_script(self.script)
+        expected_code = '@deterministic\ndef f1(a, b, c=10):\n\ta * b / c\n'
+        expected_code += '@decorator1\n@maybe_deterministic\ndef f2():\n\tdef f21(x, y=3):\n\t\tdef f211(a):\n\t\t\treturn "f211"\n\t\treturn "f21"\n\treturn "f2"\n'
+        expected_code += '@test\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n'
+        expected_code += 'main()'
+        self.assert_script_was_written_as_expected(expected_code)
+    
+    def test_add_decorator_import_maybe_deterministic_when_script_does_not_have_this_import(self):
+        with open('script_test.py', 'wt') as f:
+            f.write('@maybe_deterministic\ndef f1(a, b, c=10):\n\treturn a * b / c\n')
             f.write('@maybe_deterministic\ndef f2():\n\treturn 8\n')
-            f.write('@initialize_intpy(__file__)\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
+            f.write('@initialize_speedupy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
             f.write('main()')
         fileAST = self.getAST('script_test.py')
         functions = {'f1':fileAST.body[0],
@@ -104,35 +139,69 @@ class TestScriptService(unittest.TestCase):
                      'main':fileAST.body[2]}
         script = Script('script_test.py', fileAST, [], functions)
         
-        add_common_decorator_imports_for_execution(script)
+        add_decorator_import(script, 'maybe_deterministic')
         code1 = ast.unparse(script.AST.body[:3])
-        code2 = f"import sys\nsys.path.append('{os.getcwd()}')\nfrom speedupy.intpy import maybe_deterministic"
+        code2 = f"import sys\nsys.path.append('{os.getcwd()}')\nfrom speedupy.speedupy import maybe_deterministic"
+        code1 = self.normalize_string(code1)
+        code2 = self.normalize_string(code2)
+        self.assertEqual(code1, code2)
+    
+    def test_add_decorator_import_maybe_deterministic_when_script_already_has_this_import(self):
+        with open('script_test.py', 'wt') as f:
+            f.write('from speedupy.speedupy import maybe_deterministic\n')
+            f.write('@maybe_deterministic\ndef f1(a, b, c=10):\n\treturn a * b / c\n')
+            f.write('@maybe_deterministic\ndef f2():\n\treturn 8\n')
+            f.write('@initialize_speedupy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
+            f.write('main()')
+        fileAST = self.getAST('script_test.py')
+        import_commands = [fileAST.body[0]]
+        functions = {'f1':fileAST.body[1],
+                     'f2':fileAST.body[2],
+                     'main':fileAST.body[3]}
+        script = Script('script_test.py', fileAST, import_commands, functions)
+        
+        add_decorator_import(script, 'maybe_deterministic')
+        code1 = ast.unparse(script.AST.body[:2])
+        code2 = f"from speedupy.speedupy import maybe_deterministic\n@maybe_deterministic\ndef f1(a, b, c=10):\n\treturn a * b / c"
         code1 = self.normalize_string(code1)
         code2 = self.normalize_string(code2)
         self.assertEqual(code1, code2)
 
-    def test_add_execute_intpy_import(self):
+    def test_add_decorator_import_initialize_speedupy_when_script_does_not_have_this_import(self):
         with open('script_test.py', 'wt') as f:
-            f.write(f"import sys\nsys.path.append('{os.getcwd()}')\n")
-            f.write('from speedupy.intpy import deterministic, maybe_deterministic, collect_metadata\n')
-            f.write('@deterministic\ndef f1(a, b, c=10):\n\treturn a * b / c\n')
+            f.write('@maybe_deterministic\ndef f1(a, b, c=10):\n\treturn a * b / c\n')
             f.write('@maybe_deterministic\ndef f2():\n\treturn 8\n')
-            f.write('@collect_metadata\ndef f3():\n\treturn "f3"\n')
-            f.write('@initialize_intpy(__file__)\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
+            f.write('@initialize_speedupy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
             f.write('main()')
         fileAST = self.getAST('script_test.py')
-        functions = {'f1':fileAST.body[3],
-                     'f2':fileAST.body[4],
-                     'f3':fileAST.body[5],
-                     'main':fileAST.body[6]}
-        imports = [fileAST.body[0], fileAST.body[2]]
-        script = Script('script_test.py', fileAST, imports, functions)
+        functions = {'f1':fileAST.body[0],
+                     'f2':fileAST.body[1],
+                     'main':fileAST.body[2]}
+        script = Script('script_test.py', fileAST, [], functions)
         
-        add_execute_intpy_import(script)
-        code1 = ast.unparse(script.AST.body[:4])
-        code2 = f"import sys\nsys.path.append('{os.getcwd()}')\n"
-        code2 += 'from speedupy.intpy import deterministic, maybe_deterministic, collect_metadata\n'
-        code2 += 'from speedupy.intpy import execute_intpy'
+        add_decorator_import(script, 'initialize_speedupy')
+        code1 = ast.unparse(script.AST.body[:3])
+        code2 = f"import sys\nsys.path.append('{os.getcwd()}')\nfrom speedupy.speedupy import initialize_speedupy"
+        code1 = self.normalize_string(code1)
+        code2 = self.normalize_string(code2)
+        self.assertEqual(code1, code2)
+
+    def test_add_decorator_import_initialize_speedupy_when_script_already_has_this_import(self):
+        with open('script_test.py', 'wt') as f:
+            f.write('from speedupy.speedupy import initialize_speedupy\n')
+            f.write('@maybe_deterministic\ndef f1(a, b, c=10):\n\treturn a * b / c\n')
+            f.write('@maybe_deterministic\ndef f2():\n\treturn 8\n')
+            f.write('@initialize_speedupy\ndef main():\n\tf1(1, 2, 3)\n\tf2()\n')
+            f.write('main()')
+        fileAST = self.getAST('script_test.py')
+        import_commands = [fileAST.body[0]]
+        functions = {'f1':fileAST.body[1],
+                     'f2':fileAST.body[2],
+                     'main':fileAST.body[3]}
+        script = Script('script_test.py', fileAST, import_commands, functions)
+        add_decorator_import(script, 'initialize_speedupy')
+        code1 = ast.unparse(script.AST.body[:2])
+        code2 = f"from speedupy.speedupy import initialize_speedupy\n@maybe_deterministic\ndef f1(a, b, c=10):\n\treturn a * b / c"
         code1 = self.normalize_string(code1)
         code2 = self.normalize_string(code2)
         self.assertEqual(code1, code2)
