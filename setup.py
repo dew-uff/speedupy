934498965fac751727b55cec2285d63220c82952
JoaoLopez <joaolopez@id.uff.br>
2024-03-06 13:08:07 -0300

    Atualizando arquivo setup.py, responsável por configurar um experimento antes que ele seja executado com o SpeeduPy

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ import sys, os
 sys.path.append(os.path.dirname(__file__))
 
 from constantes import Constantes
-from entities.Experiment import Experiment
-from services.experiment_service import create_experiment, copy_experiment, create_experiment_function_graph, decorate_experiment_functions_for_execution, get_experiment_functions_hashes
+from services.experiment_service import create_experiment, overwrite_decorated_experiment, create_experiment_function_graph, decorate_experiment_functions, get_experiment_functions_hashes
 from setup_exp.environment import init_env
 from util import save_json_file, serialize_to_file
 
@@ -15,16 +14,15 @@ def validate_main_script_specified():
     if len(sys.argv) != 2:
         raise Exception('Please specify only the main script of your experiment!\nCommand: python setup.py MAIN_SCRIPT.py')
 
-def setup_experiment(main_script_path:str) -> Experiment:
+def setup_experiment(main_script_path:str) -> None:
     init_env()
     experiment = create_experiment(main_script_path)
     exp_func_graph = create_experiment_function_graph(experiment)
     functions2hashes = get_experiment_functions_hashes(exp_func_graph)
-    decorate_experiment_functions_for_execution(experiment)
-    copy_experiment(experiment)
+    decorate_experiment_functions(experiment)
+    overwrite_decorated_experiment(experiment)
     save_json_file(functions2hashes, Constantes().EXP_FUNCTIONS_FILENAME)
     serialize_to_file(experiment, Constantes().EXP_SERIALIZED_FILENAME)
-    return experiment
 
 if __name__ == '__main__':
     check_python_version()
