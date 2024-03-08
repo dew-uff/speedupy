ab6829f6b513276db46084424ba0930e191d7f11
JoaoLopez <joao.p.lopez01@gmail.com>
2024-03-08 09:52:00 -0300

    Removendo dos módulos experiment_service, function_service e script_service as funções que são utilizadas exclusivamente na fase de setup do experimento do usuário

--- services/function_service.py
+++ services/function_service.py
@@ -1,36 +1,9 @@
-from typing import List, Dict, Union, Tuple, Optional
+from typing import List, Dict, Tuple
 import ast, time
 from constantes import Constantes
 from execute_exp.data_access import get_all_saved_metadata_of_a_function_group_by_function_call_hash
 from entities.Metadata import Metadata
 from execute_exp.data_access import create_cache_entry, add_to_simulated_function_calls, add_to_dont_cache_function_calls, remove_metadata
-
-def decorate_main_function(main_function:ast.FunctionDef) -> None:
-    for decorator in main_function.decorator_list:
-        if _is_initialize_speedupy_decorator(decorator): return
-    main_function.decorator_list.append(ast.Name(id='initialize_speedupy', ctx=ast.Load()))
-
-def decorate_function(function:ast.FunctionDef) -> bool:
-    """This function checks if the function received can be decorated with @maybe_deterministic. If it is possible to do so, the function is decorated and True is returned. If the function cannot be decorated this function returns False."""
-    func_decorated = _is_already_decorated(function)
-    if not func_decorated:
-        function.decorator_list.append(ast.Name("maybe_deterministic", ast.Load()))
-    return not func_decorated
-
-def _is_already_decorated(function:ast.FunctionDef) -> bool:
-    for decorator in function.decorator_list:
-        if _is_initialize_speedupy_decorator(decorator) or \
-           _is_common_speedupy_decorator(decorator):
-            return True
-    return False
-
-def _is_initialize_speedupy_decorator(decorator:Union[ast.Call, ast.Name]) -> bool:
-    return isinstance(decorator, ast.Name) and \
-           decorator.id == "initialize_speedupy"
-
-def _is_common_speedupy_decorator(decorator:Union[ast.Call, ast.Name]) -> bool:
-    return isinstance(decorator, ast.Name) and \
-           decorator.id in ["deterministic", "maybe_deterministic"]
 
 #TODO TEST
 def execute_and_classify_function(module, function:ast.FunctionDef, functions_2_hashes:Dict[str, str]) -> None:
