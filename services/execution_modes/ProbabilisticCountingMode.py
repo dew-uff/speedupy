24ef18e1b356f280bf1d14191e2ac75f79b700ff
JoaoLopez <joaolopez@id.uff.br>
2024-03-11 10:21:28 -0300

    Substituindo consulta à classe Constantes pela classe SpeeduPySettings, que é a nova classe responsável por armazenar as configurações definidas pelo usuário ao executar o SpeeduPy.

--- services/execution_modes/ProbabilisticCountingMode.py
+++ services/execution_modes/ProbabilisticCountingMode.py
@@ -2,13 +2,13 @@ from entities.Metadata import Metadata
 from services.execution_modes.AbstractExecutionMode import AbstractExecutionMode
 from services.execution_modes.util import func_call_mode_output_occurs_enough
 from execute_exp.data_access import get_function_call_prov_entry
-from constantes import Constantes
+from execute_exp.SpeeduPySettings import SpeeduPySettings
 from pickle import dumps
 
 class ProbabilisticCountingMode(AbstractExecutionMode):
     def func_call_can_be_cached(self, func_call_hash:str) -> bool:
         return func_call_mode_output_occurs_enough(func_call_hash,
-                                                   Constantes().g_argsp_min_mode_occurrence)
+                                                   SpeeduPySettings().g_argsp_min_mode_occurrence)
 
     def get_func_call_cache(self, func_call_hash:str):
         self.__func_call_prov = get_function_call_prov_entry(func_call_hash)
