4a5a59fd1ed69a5938f8501a16d8f191277007ce
JoaoLopez <joaolopez@id.uff.br>
2024-03-14 17:34:50 -0300

    Revendo classes que representam RetrievalStrategies. Não há necessidade de testá-las.

--- execute_exp/services/retrieval_strategies/AbstractRetrievalStrategy.py
+++ execute_exp/services/retrieval_strategies/AbstractRetrievalStrategy.py
@@ -2,11 +2,12 @@ from typing import Dict, Optional
 from entities.CacheData import CacheData
 from execute_exp.services.storages.Storage import Storage
 
-#TODO: TEST
+#TODO: VERIFY IF IT IS WORTH RENAMING TO SAME NAME OF STORAGE METHODS!
+#TODO: VERIFY IF IT IS WORTH MAKING WRAPER TO SAVE_STORAGE AND MAKE ALL MEM_ARCHS ONLY INSTANTIATE RETRIEVAL_STRATEGY AND THIS INSTANCE MANAGES THE STORAGE INSTANCE
 class AbstractRetrievalStrategy():
     def __init__(self, storage:Storage):
         self._storage = storage
 
     def get_initial_cache_entries(self, use_thread=False) -> Dict[str, CacheData]: return {}
     def get_cache_entry(self, func_call_hash:str) -> Optional[CacheData]: return
-    def get_function_cache_entries(self, func_name:str, use_thread=False) -> Optional[Dict[str, CacheData]]: return
+    def get_function_cache_entries(self, func_name:str, use_thread=False) -> Dict[str, CacheData]: return {}
--- execute_exp/services/retrieval_strategies/FunctionRetrieval.py
+++ execute_exp/services/retrieval_strategies/FunctionRetrieval.py
@@ -3,17 +3,16 @@ from entities.CacheData import CacheData
 from execute_exp.services.storages.Storage import Storage
 from execute_exp.services.retrieval_strategies.AbstractRetrievalStrategy import AbstractRetrievalStrategy
 
-#TODO: TEST
 class FunctionRetrieval(AbstractRetrievalStrategy):
     def __init__(self, storage:Storage):
         super().__init__(storage)
-        self.__FUNCTIONS_ALREADY_SELECTED_FROM_DB = []
+        self.__functions_already_loaded_from_storage = []
 
     def get_cache_entry(self, func_call_hash:str) -> Optional[CacheData]:
         return self._storage.get_cached_data_of_a_function_call(func_call_hash)
     
-    def get_function_cache_entries(self, func_name:str, use_thread=False) -> Optional[Dict[str, CacheData]]:
-        if func_name in self.__FUNCTIONS_ALREADY_SELECTED_FROM_DB: return
-        self.__FUNCTIONS_ALREADY_SELECTED_FROM_DB.append(func_name)
+    def get_function_cache_entries(self, func_name:str, use_thread=False) -> Dict[str, CacheData]:
+        if func_name in self.__functions_already_loaded_from_storage: return
+        self.__functions_already_loaded_from_storage.append(func_name)
         return self._storage.get_cached_data_of_a_function(func_name,
                                                            use_isolated_connection=use_thread)
