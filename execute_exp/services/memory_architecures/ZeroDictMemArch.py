7beb2eefac971cbd5630ce485c991ce751fd3a0c
JoaoLopez <joaolopez@id.uff.br>
2024-03-15 15:22:37 -0300

    Refatorando e movendo classe ZeroDictMemArch para módulo de serviços de execute_exp/

--- execute_exp/services/memory_architecures/ZeroDictMemArch.py
+++ execute_exp/services/memory_architecures/ZeroDictMemArch.py
@@ -1,7 +1,6 @@
 from execute_exp.services.memory_architecures.AbstractMemArch import AbstractMemArch
 from entities.CacheData import CacheData
 
-#TODO: TEST
 class ZeroDictMemArch(AbstractMemArch):
     #This MemoryArchitecture can only be used with LazyRetrieval because it saves no data on RAM!
     #Thus, it always runs sequentially!
@@ -9,6 +8,6 @@ class ZeroDictMemArch(AbstractMemArch):
         try: return self._retrieval_strategy.get_cache_entry(func_call_hash).output
         except AttributeError: return
     
-    def create_cache_entry(self, func_call_hash:str, func_return, func_name=None):
+    def create_cache_entry(self, func_call_hash:str, func_return, func_name=None) -> None:
         data = CacheData(func_call_hash, func_return, func_name)
         self._storage.save_cache_data({func_call_hash: data})
