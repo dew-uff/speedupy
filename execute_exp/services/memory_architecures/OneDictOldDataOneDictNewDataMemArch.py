459bbe539400f83e1f6e4b78d728bef264da6f56
JoaoLopez <joaolopez@id.uff.br>
2024-03-15 15:20:21 -0300

    Refatorando e movendo classe OneDictOldDataOneDictNewDataMemArch para módulo de serviços de execute_exp/

--- execute_exp/services/memory_architecures/OneDictOldDataOneDictNewDataMemArch.py
+++ execute_exp/services/memory_architecures/OneDictOldDataOneDictNewDataMemArch.py
@@ -1,9 +1,9 @@
 from execute_exp.services.memory_architecures.AbstractTwoDictMemArch import AbstractTwoDictMemArch
 
-#TODO:REFACTOR
 class OneDictOldDataOneDictNewData(AbstractTwoDictMemArch):
     def _get_cache_entry_from_dict(self, func_call_hash:str):
-        c = super()._get_cache_entry_from_dict(func_call_hash)
-        if c: return c
-        if(func_call_hash in self._NEW_DATA_DICTIONARY):
+        try:
+            c = super()._get_cache_entry_from_dict(func_call_hash)
+            if c: return c
             return self._NEW_DATA_DICTIONARY[func_call_hash]
+        except KeyError: return
