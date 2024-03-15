8520540a3872b3185adf0b3eda3189a099c85ca8
JoaoLopez <joaolopez@id.uff.br>
2024-03-15 14:43:39 -0300

    Atualizando abstrações e implementações de MemoryArchitecture

--- execute_exp/services/memory_architecures/AbstractTwoDictMemArch.py
+++ execute_exp/services/memory_architecures/AbstractTwoDictMemArch.py
@@ -3,18 +3,13 @@ from execute_exp.services.retrieval_strategies.AbstractRetrievalStrategy import 
 from execute_exp.services.storages.Storage import Storage
 from entities.CacheData import CacheData
 
-#TODO: TEST
-class OneDictOneSetMemArch(AbstractOneDictMemArch):
+class AbstractTwoDictMemArch(AbstractOneDictMemArch):
     def __init__(self, storage:Storage, retrieval_strategy:AbstractRetrievalStrategy, use_threads:bool):
         super().__init__(storage, retrieval_strategy, use_threads)
-        self.__NEW_DATA = set()
-            
+        self._NEW_DATA_DICTIONARY = {}
+
     def create_cache_entry(self, func_call_hash:str, func_return, func_name=None):
-        data = CacheData(func_call_hash, func_return, func_name)
-        self.__NEW_DATA.add(data)
-        with self._DATA_DICTIONARY_SEMAPHORE:
-            self._DATA_DICTIONARY[func_call_hash] = data
+        self._NEW_DATA_DICTIONARY[func_call_hash] = CacheData(func_call_hash, func_return, func_name)
         
     def save_new_cache_entries(self):
-        data = {e.func_call_hash:e for e in self.__NEW_DATA}
-        self._storage.save_cache_data(data)
+        self._storage.save_cache_data(self._NEW_DATA_DICTIONARY)
