c6e2895f787da0be326bdeb8c573ed847db65d6a
JoaoLopez <joaolopez@id.uff.br>
2024-03-18 13:12:46 -0300

    Removendo módulo services/ que possuia classes não mais utilizadas pelo SpeeduPy

--- services/experiment_service.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict
-
-from entities.Experiment import Experiment
-from services.script_service import classify_script_functions
-
-def classify_experiment_functions(experiment:Experiment, functions_2_hashes:Dict[str, str]) -> None:
-    for script in experiment.scripts.values():
-        classify_script_functions(script, functions_2_hashes)
-    
